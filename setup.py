57f9d625032e1d1b7ab23e19ffe1509a19227b79
Timo Furrer <tuxtimo@gmail.com>
2016-11-28 14:32:10 +0100

    Fix classifiers in setup

--- setup.py
+++ setup.py
@@ -55,15 +55,9 @@ if __name__ == '__main__':
               'Operating System :: MacOS :: MacOS X',
               'Operating System :: POSIX',
               'Operating System :: POSIX :: Linux',
-              'Programming Language :: Python',
-              'Programming Language :: Python :: 2',
-              'Programming Language :: Python :: 2.7',
-              'Programming Language :: Python :: 3',
-              'Programming Language :: Python :: 3.3',
               'Programming Language :: Python :: 3.4',
               'Programming Language :: Python :: 3.5',
               'Programming Language :: Python :: Implementation',
               'Programming Language :: Python :: Implementation :: CPython',
-              'Programming Language :: Python :: Implementation :: PyPy',
           ]
     )
